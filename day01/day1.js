11e089aced1ba87fc455ba0e8da5fa5bd54eadc3
Armin Khoshbin <armin.khoshbin@gmail.com>
2015-12-15 18:25:41 +1000

    some refactoring

--- day01/day1.js
+++ day01/day1.js
@@ -2,37 +2,23 @@
 
 var fs = require('fs');
 
-var WhatFloor = function(input) {
-	// Starting point
-	var floor = 0;
-	for (var i=0; i < input.length; i++) {
-		// Count floors
-		if (input[i] === '(') floor++;
-		else if (input[i] === ')') floor--;
-	}
-	return floor;
-}
-
-var WhatPosition = function(input) {
+var What = function(input) {
 	// Starting point
 	var floor = 0,
-	position;
-	for (var i=0; i < input.length; i++) {
-		if (floor === -1) {
-			position = i;
-			// Stop counting floors on first occurance
-			break;
-		}
+	position = 0;
+	for (var i = 0; i < input.length; i++) {
+		// first basement occurance
+		if (floor === -1 && position === 0) position = i;
 		// Count floors
 		if (input[i] === '(') floor++;
 		else if (input[i] === ')') floor--;
 	}
-	return position;
+	return {'floor': floor, 'position': position};
 }
 
 fs.readFile('input.txt', function (err, data) {
   if (err) throw err;
   var input = data.toString();
-  console.log('Santa is on floor ' + WhatFloor(input));
-  console.log('Santa enters basement for the first time at position ' + WhatPosition(input));
+  console.log('Santa is on floor ' + What(input).floor);
+  console.log('Santa enters basement for the first time at position ' + What(input).position);
 });
