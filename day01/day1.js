030278eb8e0a793cbe40ad04f9459fb84fdd8868
Armin Khoshbin <armin.khoshbin@gmail.com>
2015-12-15 18:00:49 +1000

    part 2 of day1 challenge

--- day01/day1.js
+++ day01/day1.js
@@ -3,17 +3,43 @@
 var fs = require('fs');
 
 var WhatFloor = function(input) {
-	// Base floor as starting point
-	var floor = 0;
+	// Starting point
+	var floor = 0,
+	basement = false,
+	position;
 	for (var i=0; i < input.length; i++) {
+		if (floor === -1) basement = !basement;
+		if (basement) {
+			position = i+1;
+			basement = null;
+		}
+		// Count floors
 		if (input[i] === '(') floor++;
 		else if (input[i] === ')') floor--;
 	}
 	return floor;
 }
 
+var WhatPosition = function(input) {
+	// Starting point
+	var floor = 0,
+	position;
+	for (var i=0; i < input.length; i++) {
+		if (floor === -1) {
+			position = i;
+			// Stop counting floors on first occurance
+			break;
+		}
+		// Count floors
+		if (input[i] === '(') floor++;
+		else if (input[i] === ')') floor--;
+	}
+	return position;
+}
+
 fs.readFile('input.txt', function (err, data) {
   if (err) throw err;
   var input = data.toString();
   console.log('Santa is on floor ' + WhatFloor(input));
+  console.log('Santa enters basement for the first time at position ' + WhatPosition(input));
 });
