e7e827c0c0dfbade8364a7af453e1eed1df636c0
Armin Khoshbin <armin.khoshbin@gmail.com>
2015-12-19 22:59:16 +1000

    day02 completed

--- day02/day02.js
+++ day02/day02.js
@@ -2,8 +2,9 @@
 
 var fs = require('fs');
 
-var TotalWrapper = function(input) {
+var Total = function(input) {
 	var totalWrap = 0;
+	var totalRibbon = 0;
 	for (var i = 0; i < input.length; i++) {
 		var dimensions = input[i].split("x");
 		for (var j = 0; j < dimensions.length; j++) {
@@ -12,14 +13,16 @@ var TotalWrapper = function(input) {
 		// JS doesn't have a out of a box function for sorting numerical elements?!
 		dimensions = dimensions.sort(function(a, b) {return a-b} );
 		totalWrap += (2 * (dimensions[0]*dimensions[1] + dimensions[0]*dimensions[2] + dimensions[1]*dimensions[2])) + (dimensions[0]*dimensions[1]);
+		totalRibbon += (2 * (dimensions[0] + dimensions[1])) + (dimensions[0] * dimensions[1] * dimensions[2]);
 		//console.log(dimensions);
 	}
-	return totalWrap;
+	return {'totalWrap': totalWrap, 'totalRibbon': totalRibbon};
 }
 
 fs.readFile('input.txt', function (err, data) {
   if (err) throw err;
   var input = data.toString();
   var array = input.split("\n");
-  console.log('Total of ' + TotalWrapper(array) + ' square feet of wrapping paper is needed.');
+  console.log('Total of ' + Total(array).totalWrap + ' square feet of wrapping paper is needed.');
+  console.log('Total of ' + Total(array).totalRibbon + ' square feet of ribbon is needed.');
 });
